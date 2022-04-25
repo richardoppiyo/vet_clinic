27bc0ede46eb7229cceed86074ba02e1b0fe197b
Richard Opiyo <romenyo@dewcis.com>
2022-04-26 01:29:38 +0300

    create new tables and add forwign key constraint

--- schema.sql
+++ schema.sql
@@ -9,3 +9,40 @@ CREATE TABLE animals(
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR;
+
+
+CREATE TABLE owners(
+	id				INT GENERATED ALWAYS AS IDENTITY,
+	full_name		VARCHAR,
+	age				INT,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+	id 				INT GENERATED ALWAYS AS IDENTITY,
+	name 			VARCHAR,
+	PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER;
+
+ALTER TABLE animals 
+ADD CONSTRAINT fk_species_id 
+FOREIGN KEY (species_id ) 
+REFERENCES species (id);
+
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER;
+
+ALTER TABLE animals 
+ADD CONSTRAINT fk_owner_id
+FOREIGN KEY (owner_id) 
+REFERENCES owners (id);
+
+
